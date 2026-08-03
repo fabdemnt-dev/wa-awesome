c4669ca27576a488ed2dfb1ee15a37a38088adb9
fabdemnt-dev <fabdemnt@gmail.com>
2026-08-04 00:55:51 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtb74uz6clsoc9uA_AkDHi7DdepEWn2dw",
+  authDomain: "wa-awesome.firebaseapp.com",
+  projectId: "wa-awesome",
+  storageBucket: "wa-awesome.firebasestorage.app",
+  messagingSenderId: "1074804319870",
+  appId: "1:1074804319870:web:923f0ec866812f98ae5a2f",
+  measurementId: "G-DH30YVYB2X"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
