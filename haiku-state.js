0befbb777a06978ad1dcbbe87d4aada152243df8
fabdemnt-dev <fabdemnt@gmail.com>
2026-08-05 15:14:06 +0900

    Create haiku-state.js

---
+++ haiku-state.js
@@ -0,0 +1,17 @@
+const state = {
+  roomId: "",
+  myName: "",
+  isSpectator: false,
+  roomRef: null,
+
+  myHand5: [],
+  myHand7: [],
+  selectedHand: [null, null, null],
+
+  currentData: null,
+
+  isSubmittingSelfPraise: false,
+  isProcessingNextRound: false
+};
+
+export default state;
