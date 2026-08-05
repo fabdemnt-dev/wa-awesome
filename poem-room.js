1fd8b6479a4143546921dc92c209f05aa1a5a35e
fabdemnt-dev <fabdemnt@gmail.com>
2026-08-05 23:18:57 +0900

    Add getDoc import to poem-room.js

--- poem-room.js
+++ poem-room.js
@@ -1,5 +1,5 @@
 import { db } from "./firebase-config.js";
-import { doc, setDoc, onSnapshot, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { doc, getDoc, setDoc, onSnapshot, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import state from './poem-state.js';
 import { escapeHTML, escapeJS, renderInputFields, renderHand, renderBoards } from './poem-render.js';
 import { setupAutoResize } from './poem-action.js';
